96ed7135601e203ae0b2cca26d6a4c92d60827c2
smjeffery22 <parkjon8@gmail.com>
2022-05-08 23:29:54 -0400

    C# fundamentals for beginner

--- c#/c#_fund_abs_beg/Decisions/Decisions/Program.cs
+++ c#/c#_fund_abs_beg/Decisions/Decisions/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Bob's Big Giveaway");
+//Console.Write("Choose a door: 1, 2 or 3: ");
 
+//string userValue = Console.ReadLine();
+//string message = "";
+
+//if (userValue == "1")
+//{
+//    message = "You won a new car!";
+
+//}
+//else if (userValue == "2")
+//{
+//    message = "You won a new boat!";
+
+//}
+//else if (userValue == "3")
+//{
+//    message = "You won a new cat!";
+//}
+//else
+//{
+//    message = "Sorry, we didn't understand.";
+//}
+
+//Console.WriteLine(message);
+
+//Console.ReadLine();
+
+Console.WriteLine("Bob's Big Giveaway");
+Console.Write("Choose a door: 1, 2 or 3: ");
+
+string userValue = Console.ReadLine();
+string message = (userValue == "1") ? "boat" : "strand of lint";
+
+//Console.Write("You won a ");
+//Console.Write(message);
+//Console.Write(".");
+
+Console.WriteLine("You entered: {0} and yon won a {1}.", userValue, message);
---
+++ c#/c#_fund_abs_beg/ForIteration/ForIteration/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int i = 0; i < 10; i++)
+{
+if (i == 7)
+    {
+        Console.WriteLine("Found Seven!");
+        break;
+    }
+}
+
+for (int j = 0; j < 12; j++)
+{
+    Console.WriteLine(j);
+}
